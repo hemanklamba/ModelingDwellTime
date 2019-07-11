4cfba1057538a8b538d3b245497913d83a15a46e
Hemank Lamba <hemank08025@iiitd.ac.in>
2019-07-10 22:46:01 -0400

    Copula Fitting files

---
+++ RCopula_Fitting_VIEWERS.R
@@ -0,0 +1,62 @@
+library(VineCopula)
+library(fitdistrplus)
+library(ks)
+library(MASS)
+
+size=30
+trainFile='Train Data Parameters'
+testFile='Test Data Parameters'
+trainProbsFile='File containing probability for observing each train instance according to the ind. models'
+testProbsFile='File containing probability for observing each test instance according to the ind. models'
+
+trainData <- read.csv(trainFile, sep=' ', header=FALSE)
+testData <- read.csv(testFile, sep=' ', header=FALSE)
+trainProbs <- read.csv(trainProbsFile, sep=' ', header=FALSE)
+testProbs <- read.csv(testProbsFile, sep=' ', header=FALSE)
+
+trainData <- as.matrix(trainData)
+testData <- as.matrix(testData)
+trainProbs <- as.matrix(trainProbs)
+testProbs <- as.matrix(testProbs)
+
+cvine <- RVineStructureSelect(trainData, type=1)
+ll <- RVineLogLik(trainData, cvine)$loglik
+
+pdf <- RVinePDF(trainData, cvine)
+PDF <- pdf * trainProbs[,1] * trainProbs[,2] * trainProbs[,3] * trainProbs[,4] * trainProbs[,5] * trainProbs[,6]
+
+nrow = dim(trainData)[2]
+ncol = dim(trainData)[2]
+pdf("Pair Plot over Parameters for training instances", height=100, width=100)
+pdf(pdf_file, height=100, width=100)
+par(mfrow=c(nrow, ncol), cex=0.5, mai=c(0.1, 0.1, 0.1, 0.1))
+par("mar")
+par(mar=c(1,1,1,1))
+for(row in 1:ncol)
+  for(col in 1:ncol)
+    if(row<=col){
+      plot.new()
+      text(0.5, 0.5, cor(trainData[,row], trainData[,col]), cex=16)} else{
+        fhat <- kde(x = trainData[,c(row,col)])
+        #plot(mod_data[,row], mod_data[,col],cex=0.1)
+        plot(fhat, display="filled.contour2", cont=seq(10,90, by=10))}
+dev.off()
+
+sim_data <- RVineSim(100000, cvine)
+# Save sim_data to file
+write.table(sim_data, file="Simulated Data from Fitted Copula", sep=" ", col.names = F, row.names = F)
+
+pdf("Pair PDF Plots over Sampled Instances after Copula Fitting", height=100, width=100)
+pdf(pdf_file, height=100, width=100)
+par(mfrow=c(nrow, ncol), cex=0.5, mai=c(0.1, 0.1, 0.1, 0.1))
+par("mar")
+par(mar=c(1,1,1,1))
+for(row in 1:ncol)
+  for(col in 1:ncol)
+    if(row<=col){
+      plot.new()
+      text(0.5, 0.5, cor(sim_data[,row], sim_data[,col]), cex=16)} else{
+        fhat <- kde(x = sim_data[,c(row,col)])
+        #plot(mod_data[,row], mod_data[,col],cex=0.1)
+        plot(fhat, display="filled.contour2", cont=seq(10,90, by=10))}
+dev.off()
